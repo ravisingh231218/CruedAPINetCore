c1c0e0235993f01f3d61a8ba3ffa9441b99611af
Parvej Ali <parvej291710@gmail.com>
2023-09-15 15:51:33 +0530

    Add project files.

---
+++ CruedAPINetCore/Controllers/BrandController.cs
@@ -0,0 +1,110 @@
+﻿using CruedAPINetCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CruedAPINetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly BrandContext _dbContext;
+
+        public BrandController(BrandContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
+        {
+            if(_dbContext.Brands == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Brands.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Brand>> GetBrands(int id)
+        {
+            if (_dbContext.Brands == null)
+            {
+
+                return NotFound();
+            }
+            var brand = await _dbContext.Brands.FindAsync(id);
+            if(brand == null) 
+            {
+                return NotFound();
+            }
+            return brand;                       
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
+        {
+            _dbContext.Brands.Add(brand);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetBrands), new { id = brand.Id }, brand);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutBrand(int id, Brand brand)
+        {
+            if (id != GetID(brand))
+            {
+                return BadRequest();
+            }
+            _dbContext.Entry(brand).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BrandAvailble(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok();
+
+            static int GetID(Brand brand)
+            {
+                return brand.ID;
+            }
+        }
+        private bool BrandAvailble(int id)
+        {
+            return (_dbContext.Brands?.Any(x => x.ID == id)).GetValueOrDefault();
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            if(_dbContext.Brands == null)
+            {
+                return NotFound();
+            }
+            var brand = await _dbContext.Brands.FindAsync(id);
+            if(brand == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Brands.Remove(brand);
+
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+
+    }
+}
---
+++ CruedAPINetCore/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿namespace CruedAPINetCore.Models
+{
+    public class Brand
+    {
+        [key]
+
+        public int Id { get; set; }
+        public int ID { get; internal set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public int IsActive { get; set;}
+    }
+}
